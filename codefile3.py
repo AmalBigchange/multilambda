15af2dce6a69dee425a4bf1f7628ca41ef027441
Amal <Amal.bigchange@gmail.com>
2025-02-16 15:08:19 +0000

    multilambda creation added. YAML file also added

---
+++ codefile3.py
@@ -0,0 +1,18 @@
+import logging
+import json
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    logger.info("This is a test log for info")
+    logger.error("This is a test log for error")
+    logger.error("Testing CI/CD")
+
+    logger.error("Testing if the lambda function works")
+
+
+    return {
+        'statusCode': 200,
+        'body': 'Hello from Lambda v5!'
+    }
--- main.tf
+++ main.tf
@@ -7,12 +7,28 @@ module "s3_code_upload" {
   
 }
 
+module "s3_code_upload2" {
+    source = "./modules/s3" # this is the path to the s3 module folder
+    #bucket_name = "bucket.name" /use this line to give it a custom bucket name if not it will use the default 
+    source_file = "./codefile3.py"
+    output_path = "lambda_2${formatdate("YYYYMMDDHHmmss", timestamp())}.zip"
+  
+}
+
 #module to call the lambda main.tf to create a lambda object
 module "lambda_code" {
     source = "./modules/lambda"
     function_name = "mylambda2"
     s3_bucket = "s3amal"
     s3_key = module.s3_code_upload.output_path
+    runtime = "python3.9"
+    handler = "lambda_code.handler"
+}
+module "lambda_code2" {
+    source = "./modules/lambda"
+    function_name = "mylambda3"
+    s3_bucket = "s3amal"
+    s3_key = module.s3_code_upload2.output_path
     runtime = "python3.9"
     handler = "lambda_code.handler"
 }
