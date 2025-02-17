b6f595edbf5a3df6769c301419cff78170b4819e
Amal <Amal.bigchange@gmail.com>
2025-02-17 03:25:29 +0000

    output path deleted

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,4 +1,4 @@
-output "output_path" {
+/*output "output_path" {
   value = "lambda_${formatdate("YYYYMMDDHHmmss", timestamp())}.zip"
   description = "The output path of the zipped source code file."
-}
+}*/
